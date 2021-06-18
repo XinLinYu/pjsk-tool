2cd503b980714bd8cd9f15fed8a90281c1a3ea71
AsadaKintoki <35424840+AsadaKintoki@users.noreply.github.com>
2021-06-18 00:35:58 -0400

    add sendMassege function to Pjsk handle module

--- src/main/java/com/xinlindeyu/controller/PjskListener.java
+++ src/main/java/com/xinlindeyu/controller/PjskListener.java
@@ -21,7 +21,7 @@ public class PjskListener implements Consumer<GroupMessageEvent> {
             "pjsk", "/pjsk"
     };
 
-    private void stepCmd(Group api, long groupId, long qq, String message) {
+    private void stepCmd(Group api, long groupId, long qq, String message, GroupMessageEvent groupMessageEvent) {
         boolean isCMD = false;
         for (String s : CMD) {
             if (message.contains(s)) {
@@ -32,10 +32,16 @@ public class PjskListener implements Consumer<GroupMessageEvent> {
         if (!isCMD) {
             return;
         }
-        log.info("《世界计划 彩色舞台 feat. 初音未来》（日语：プロジェクトセカイ カラフルステージ! feat. 初音ミク；英语：Project Sekai Colorful Stage! feat. Hatsune Miku）是由SEGA、Craft Egg、Crypton Future Media共同企划，SEGA Games与Craft Egg子公司Colorful Palette共同制作的一款音乐节奏和视觉小说类的社交手机卡牌网络游戏，于2020年9月30日在日本App Store和Google Play商店正式上线。官方简称“プロセカ”，国内粉丝简称“PJSK（啤酒烧烤）”。\n" +
+        
+        String info_pjsk = "《世界计划 彩色舞台 feat. 初音未来》（日语：プロジェクトセカイ カラフルステージ! feat. 初音ミク；英语：Project Sekai Colorful Stage! feat. Hatsune Miku）是由SEGA、Craft Egg、Crypton Future Media共同企划，SEGA Games与Craft Egg子公司Colorful Palette共同制作的一款音乐节奏和视觉小说类的社交手机卡牌网络游戏，于2020年9月30日在日本App Store和Google Play商店正式上线。官方简称“プロセカ”，国内粉丝简称“PJSK（啤酒烧烤）”。\n" +
                 "\n" +
                 "阅读更多：世界计划_彩色舞台_feat._初音未来（https://zh.moegirl.org.cn/%E4%B8%96%E7%95%8C%E8%AE%A1%E5%88%92_%E5%BD%A9%E8%89%B2%E8%88%9E%E5%8F%B0_feat._%E5%88%9D%E9%9F%B3%E6%9C%AA%E6%9D%A5 ）\n" +
-                "本文引自萌娘百科(https://zh.moegirl.org.cn )，文字内容默认使用《知识共享 署名-非商业性使用-相同方式共享 3.0》协议。");
+                "本文引自萌娘百科(https://zh.moegirl.org.cn )，文字内容默认使用《知识共享 署名-非商业性使用-相同方式共享 3.0》协议。";
+        
+        log.info(info_pjsk);
+        
+        groupMessageEvent.getSubject().sendMessage(info_pjsk);
+        
     }
 
     @Override
@@ -47,7 +53,8 @@ public class PjskListener implements Consumer<GroupMessageEvent> {
         log.info(message);
         long groupId = group.getId();
         long qq = groupMessageEvent.getSender().getId();
-        stepCmd(group, groupId, qq, message);
+        
+        stepCmd(group, groupId, qq, message, groupMessageEvent);
     }
 
     @NotNull
